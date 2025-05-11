a12bf52377c418519f97d56ac24e71e5f0e2b63d
Yash Kumar <131612502+yaashuzi@users.noreply.github.com>
2025-05-12 02:47:41 +0530

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomNumber1 = Math.floor(Math.random() * 6)  + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
